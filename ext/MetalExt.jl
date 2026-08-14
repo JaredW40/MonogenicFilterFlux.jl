e2f01a16ca4a91ca3741fecf0267919bc7b7e2dc
JaredW40 <trumpetpro40@gmail.com>
2026-08-13 17:29:07 -0700

    Define AdjointStyle for MtlFFTPlan; fixes Metal gradient parity test.

--- ext/MetalExt.jl
+++ ext/MetalExt.jl
@@ -9,10 +9,12 @@ using Adapt
 using AbstractFFTs, FFTW
 import MLDataDevices: MetalDevice, CPUDevice
 
+AbstractFFTs.AdjointStyle(::Metal.MtlFFTPlan) = AbstractFFTs.FFTAdjointStyle()
+ 
 _gpuPlan(p::Nothing, region) = nothing
 _gpuPlan(p::Tuple, region) = map(pi -> _gpuPlan(pi, region), p)
 _gpuPlan(p, region) = plan_fft(Metal.zeros(complex(eltype(p)), size(p)...), region)
-
+ 
 function _checkMetalPrecision(m::MonoConvFFT)
     real(eltype(m.weight)) === Float64 && error(
         "MonoConvFFT: this layer's filter bank is Float64 and Metal has no " *
@@ -42,7 +44,7 @@ end
 # Already on the device.
 Adapt.adapt_structure(::MetalDevice,
     m::MonoConvFFT{D,OT,F,A,PD,P,T,S,AL}) where {D,OT,F,A<:MtlArray,PD,P,T,S,AL} = m
-
+ 
 # `A<:MtlArray` is what lets this coexist with CUDAExt's `::CPUDevice` method.
 function Adapt.adapt_structure(::CPUDevice,
     m::MonoConvFFT{D,OT,F,A,PD,P,T,S,AL}) where {D,OT,F,A<:MtlArray,PD,P,T,S,AL}
@@ -54,7 +56,7 @@ end
 Adapt.adapt_structure(::Type{<:MtlArray}, m::MonoConvFFT) = Adapt.adapt_structure(MetalDevice(), m)
 Adapt.adapt_structure(::Type{<:Array}, m::MonoConvFFT{D,OT,F,A}) where {D,OT,F,A<:MtlArray} =
     Adapt.adapt_structure(CPUDevice(), m)
-
+ 
 Metal.mtl(m::MonoConvFFT) = Adapt.adapt_structure(MetalDevice(), m)
-
+ 
 end # module
